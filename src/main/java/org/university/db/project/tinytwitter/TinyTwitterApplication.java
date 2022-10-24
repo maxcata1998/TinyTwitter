b91b20c19d0d3cb3f2b50b305e4d78a7783cd961
yan.ti <yan.ti@northeastern.edu>
2022-10-24 13:10:37 -0400

    init repo

---
+++ src/main/java/org/university/db/project/tinytwitter/TinyTwitterApplication.java
@@ -0,0 +1,13 @@
+package org.university.db.project.tinytwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyTwitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinyTwitterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/university/db/project/tinytwitter/TinyTwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.university.db.project.tinytwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyTwitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
